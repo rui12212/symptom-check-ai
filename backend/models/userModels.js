375f32d69cdb9d53e894d619a06f838986a45dbe
rui12212 <kruike221@gmail.com>
2025-06-24 23:41:08 +0300

    api models mkdir

---
+++ backend/models/userModels.js
@@ -0,0 +1,20 @@
+DB操作
+const db = require('../db/connection');
+
+async function findUserByEmail(email){
+    const [rows] = await db.execute('SELECT * FROM users WHERE email = ?',[email]);
+    return rows[0];
+}
+
+async function createUser(email,passwordHash, occupation, gender, dateObBirth){
+    const [result] = await db.execute(
+        'INSERT INTO users (email, password_hash, occupation, gender, date_of_birth) VALUES(?,?,?,?,?)',
+        [email, passwordHash, occupation, gender, dateOfBirth]
+    );
+    return result.insertId
+}
+
+module.exports = {
+    findUserByEmail,
+    createUser,
+}
