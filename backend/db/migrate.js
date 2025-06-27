6bffda88ef1aa02d2848e469e44c7eca3c68758c
rui12212 <kruike221@gmail.com>
2025-06-27 23:44:32 +0300

    backend/routes/auth.js

--- backend/db/migrate.js
+++ backend/db/migrate.js
@@ -6,7 +6,7 @@ async function migrate(){
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.nextTick.DB_USER,
-        password: process.env.DB_PASSOWRD,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
     });
     
